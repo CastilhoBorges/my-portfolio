3a263ab0eafea8b638512cb3f63a638eace48d9c
CastilhoBorges <augustocastilhoborges21@gmail.com>
2024-05-24 16:32:39 -0300

    SaveTwo

--- js/script.js
+++ js/script.js
@@ -16,10 +16,19 @@ if (navClose) { // Fechar o menu
 }
 
 /* ====== Remover o menu quando telas maiores ====== */
-const navLink = document.querySelector('.nav_link')
+const navLink = document.querySelectorAll('.nav_link')
 
-const linkaction = () =>{
+const linkAction = () =>{
     const navMenu = document.querySelector('#menu-bar')
     navMenu.classList.remove('show-menu')
 }
-navLink.forEach(n => n.addEventListener('click', linkaction))
+navLink.forEach(n => n.addEventListener('click', linkAction))
+
+/* ====== Adicionar blur ao header ====== */
+const blurHeader = () => {
+    const header = document.querySelector('#header')
+
+    this.scrollY >= 50 ? header.classList.add('blur-header')
+                       : header.classList.remove('blur-header')
+}
+window.addEventListener('scroll', blurHeader)
