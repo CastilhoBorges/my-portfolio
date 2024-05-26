4ce31e06474a998e34c5ce031a4176488819e97e
CastilhoBorges <augustocastilhoborges21@gmail.com>
2024-05-26 20:02:20 -0300

    Finalizando

--- js/script.js
+++ js/script.js
@@ -34,25 +34,57 @@ const blurHeader = () => {
 window.addEventListener('scroll', blurHeader)
 
 /* ====== Email JS ====== */
-const contatoForm = document.getElementById('contato-form'),
-      contatoMsg = document.getElementById('contato-mensagem')
+const contatoForm = document.querySelector('#contato-form'),
+      contatoMessage = document.querySelector('#contato-mensagem')
 
 const sendEmail = (e) =>{
     e.preventDefault()
 
-    emailjs.sendForm('service_3en4srr', 'template_pbnnknx', '#contato-form', 'y6fBI962t31HU3p8t')
-        .then(() =>{
-            contatoMsg.textContent = 'Mensagem enviada com suceeso ✅'
-
+    emailjs.sendForm('service_3en4srr', 'template_kd4wlwa', '#contato-form', 'y6fBI962t31HU3p8t')
+        .then(()=>{
+            contatoMessage.textContent = 'Mensagem enviada com sucesso ✅'
+            
+            // Eliminar a mensagem de enviado ou erro apos 5s 
             setTimeout(() =>{
-                contatoMsg.textContent = ''
+                contatoMessage.textContent = ''
             }, 5000)
-
+            
+            // Para limpar apos enviado 
             contatoForm.reset()
 
         }, () =>{
-            contatoMsg.textContent = 'Mensagem não enviada (erro de serviço) ❌'
+            contatoMessage.textContent = 'Mensagem não enviada (Erro de serviço) ❌'
         })
 }
 
 contatoForm.addEventListener('submit', sendEmail)
+
+/* ====== Scroll Up ====== */
+const scrollUp = () =>{
+    const scrollUp = document.querySelector('#scroll-up')
+    this.scrollY >= 350 ? scrollUp.classList.add('show-scroll')
+                    : scrollUp.classList.remove('show-scroll')
+}
+window.addEventListener('scroll', scrollUp)
+
+/* ====== Scroll Section Links ====== */    
+const sections = document.querySelectorAll('section[id]')
+
+const scrollActive = () =>{
+    const scrollY = window.pageYOffset
+
+    sections.forEach(current =>{
+        const sectionHeight = current.offsetHeight,
+              sectionTop = current.offsetTop - 58,
+              sectionId = current.getAttribute('id'),
+              sectionClass = document.querySelector('.menu_bar a[href*=' + sectionId + ']')
+
+        if(scrollY > sectionTop && scrollY <= sectionTop + sectionHeight){
+            sectionClass.classList.add('active-link')
+        } else {
+            sectionClass.classList.remove('active-link')
+        }
+    })
+
+}
+window.addEventListener('scroll', scrollActive)
